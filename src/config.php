7c58d7b25ef541756f34ce3af9b5daa952925c9e
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-02-28 14:27:16 +0530

    done

--- src/config.php
+++ src/config.php
@@ -1,13 +1,142 @@
 <?php
-session_start()
+session_start();
+ 
+//  session_destroy();
+
+
 ?>
 <?php
-include 'products.php';
+if(isset($_POST['addCart101']))
+{
+    
+$product_image=$_POST['image101'];
+$product_title=$_POST['title101'];
+$product_price=$_POST['price101'];
+$product_quantity=$_POST['quantity101'];
 
-$cart=($image,$title,$price);
+$_SESSION['cart'][]=array('p_image' => $product_image ,
+                          'p_title' =>$product_title ,
+                          'p_price' =>$product_price ,
+                          'p_quantity' => $product_quantity ,
+);
 
 
+ }
+ if(isset($_POST['addCart102']))
+{
+    
+$product_image=$_POST['image102'];
+$product_title=$_POST['title102'];
+$product_price=$_POST['price102'];
+$product_quantity=$_POST['quantity102'];
+
+$_SESSION['cart'][]=array('p_image' => $product_image ,
+                          'p_title' =>$product_title ,
+                          'p_price' =>$product_price ,
+                          'p_quantity' => $product_quantity ,
+);
 
 
+ }
+ if(isset($_POST['addCart103']))
+{
+    
+$product_image=$_POST['image103'];
+$product_title=$_POST['title103'];
+$product_price=$_POST['price103'];
+$product_quantity=$_POST['quantity103'];
 
+$_SESSION['cart'][]=array('p_image' => $product_image ,
+                          'p_title' =>$product_title ,
+                          'p_price' =>$product_price ,
+                          'p_quantity' => $product_quantity ,
+);
+
+
+ }
+ if(isset($_POST['addCart104']))
+{
+    
+$product_image=$_POST['image104'];
+$product_title=$_POST['title104'];
+$product_price=$_POST['price104'];
+$product_quantity=$_POST['quantity101'];
+
+$_SESSION['cart'][]=array('p_image' => $product_image ,
+                          'p_title' =>$product_title ,
+                          'p_price' =>$product_price ,
+                          'p_quantity' => $product_quantity ,
+);
+
+
+ }
+ if(isset($_POST['addCart105']))
+{
+    
+$product_image=$_POST['image105'];
+$product_title=$_POST['title105'];
+$product_price=$_POST['price105'];
+$product_quantity=$_POST['quantity105'];
+
+$_SESSION['cart'][]=array('p_image' => $product_image ,
+                          'p_title' =>$product_title ,
+                          'p_price' =>$product_price ,
+                          'p_quantity' => $product_quantity ,
+);
+
+
+ }
 ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>viewCart</title>
+ </head>
+ <body>
+ <?php
+
+include 'products.php';
+ ?>
+     <div class="container">
+         <div class="row">             <div class="col-lg-12">
+                <h1>MY CART</h1>
+            </div>
+        </div>
+     </div>
+     <div class="container-fluid">
+         <div class="row">
+             <div class="col">
+                <table class="table table-borderblack text-center" >
+                                            <th>INDEX</th>
+                         <th>IMAGE</th>
+                         <th>TITLE</th>
+                        <th>PRICE</th>
+                        <th>QUANTITY</th>
+                    
+                     <tbody>
+                         <?php
+                        
+                        
+                         if(isset($_SESSION['cart']))
+                        {
+foreach ($_SESSION['cart'] as $key=> $value)
+{            echo" <tr>
+            <td>$key</td>
+               <td>$value[p_image]</td>
+               <td>$value[p_title]</td>
+                <td>$value[p_price]</td>
+                <td>$value[p_quantity]</td>
+                 </tr>";
+}
+ }
+ ?>
+ </tbody> </table>
+</div>
+</div>
+ </div>
+    
+</body>
+</html>
--- src/products.php
+++ src/products.php
@@ -11,71 +11,103 @@
 	
 	<div id="main">
 		<div id="products">
+            <form action='config.php' method='POST'>
 			<div id="product-101" class="product">
                 <?php
 				$img1='<img src="images/football.png">';
 				$title1='<h3 class="title"><a href="#">Product 101</a></h3>';
 				$price1='<span>Price: $150.00</span>';
-				$button1='<a class="add-to-cart" href="#">Add To Cart</a> <i class="fa fa-cart-plus" aria-hidden="true"></i>';
+                
+                
+                
+				
                 $product1=array($img1,$title1,$price1,$button1);
                 foreach($product1 as $val1)
                 {
                     echo $val1;
                 }
                 ?>
+                <input type='hidden' name='title101' value='101'>
+                <input type='hidden' name='price101' value='$150.00'>
+                <input type='hidden' name='image101' value='<img src="images/football.png">'>
+                <input type='hidden' name='quantity101' value='1'>
+                <input type="submit" name="addCart101" class="add-to-cart" value="Add To Cart"> 
+				
 			</div>
-			<div id="product-101" class="product">
+			<div id="product-102" class="product">
             <?php
 			$img2 =	'<img src="images/tennis.png">';
             $title2='<h3 class="title"><a href="#">Product 102</a></h3>';
             $price2='<span>Price: $120.00</span>';
-            $button2='<a class="add-to-cart" href="#">Add To Cart</a>';
+           
                 $product2=array($img2,$title2, $price2,$button2);
                 foreach($product2 as $val2)
                 {
                     echo $val2;
                 }
                 ?>
+                <input type='hidden' name='title102' value='102'>
+                <input type='hidden' name='price102' value=' $120.00'>
+                <input type='hidden' name='image102' value='<img src="images/tennis.png">'>
+                <input type='hidden' name='quantity102' value='1'>
+                <input type="submit" name="addCart102" class="add-to-cart" value="Add To Cart"> 
 			</div>
-			<div id="product-101" class="product">
+			<div id="product-103" class="product">
             <?php
 				$img3='<img src="images/basketball.png">';
 				$title3='<h3 class="title"><a href="#">Product 103</a></h3>';
 				$price3='<span>Price: $90.00</span>';
-				$button3='<a class="add-to-cart" href="#">Add To Cart</a>';
+				
                 $product3=array($img3,$title3, $price3,$button3);
                 foreach($product3 as $val3)
                 {
                     echo $val3;
                 }
+            
                 ?>
+                <input type='hidden' name='title103' value='103'>
+                <input type='hidden' name='price103' value='$90.00'>
+                <input type='hidden' name='image103' value='<img src="images/basketball.png">'>
+                <input type='hidden' name='quantity103' value='1'>
+                <input type="submit" name="addCart103" class="add-to-cart" value="Add To Cart"> 
 			</div>
-			<div id="product-101" class="product">
+			<div id="product-104" class="product">
             <?php
 				$img4='<img src="images/table-tennis.png">';
 				$title4='<h3 class="title"><a href="#">Product 104</a></h3>';
 				$price4='<span>Price: $110.00</span>';
-				$button4='<a class="add-to-cart" href="#">Add To Cart</a>';
+				
                 $product4=array($img4,$title4, $price4,$button4);
                 foreach($product4 as $val4)
                 {
                     echo $val4;
                 }
                 ?>
+                <input type='hidden' name='title104' value='104'>
+                <input type='hidden' name='price104' value='$110.00'>
+                <input type='hidden' name='image104' value='<img src="images/table-tennis.png">'>
+                <input type='hidden' name='quantity104' value='1'>
+                <input type="submit" name="addCart104" class="add-to-cart" value="Add To Cart"> 
 			</div>
-			<div id="product-101" class="product">
+			<div id="product-105" class="product">
             <?php
 				$img5='<img src="images/soccer.png">';
 				$title5='<h3 class="title"><a href="#">Product 105</a></h3>';
 				$price5='<span>Price: $80.00</span>';
-				$button5='<a class="add-to-cart" href="#">Add To Cart</a>';
+				
                 $product5=array($img5,$title5, $price5,$button5);
                 foreach($product5 as $val5)
                 {
                     echo $val5;
                 }
                 ?>
+                <input type='hidden' name='title105' value='105'>
+                <input type='hidden' name='price105' value='$80.00'>
+                <input type='hidden' name='image105' value='<img src="images/soccer.png">'>
+                <input type='hidden' name='quantity105' value='1'>
+                <input type="submit" name="addCart105" class="add-to-cart" value="Add To Cart"> 
 			</div>
+            </form>
 		</div>
 	</div>
 	<?php include 'footer.php';?>
